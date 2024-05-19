9c0a4dda494e3e864b0d1d9163045901b10ce175
ZeynepAzap <zeynepazap@icloud.com>
2024-05-19 18:26:50 +0300

    12
    Son düzenlemeleri ve proje raporunu yaptım.

--- Web Proje/Iletisim.php
+++ Web Proje/Iletisim.php
@@ -3,12 +3,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
+    $gender =$_POST['gender'];
 
-    // Form verilerini işleme veya başka bir işlem yapma
-    // Örneğin, veritabanına kaydetme gibi işlemler yapılabilir.
+
 
     // Örnek bir geri dönüş
-    echo "Form gönderildi! Ad Soyad: $name, E-posta: $email, Mesaj: $message";
+    echo "Form gönderildi! Ad Soyad: $name, E-posta: $email, Mesaj: $message, Cinsiyet: $gender";
 } else {
     echo "Geçersiz istek!";
 }
